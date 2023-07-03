9981674e35c714c02efc2ad67a64898cc4a52c84
ReeceRiley-aa <rhyeca.riley@aa.com>
2023-07-03 09:46:56 -0700

    Test ""

--- version-bump.sh
+++ version-bump.sh
@@ -45,7 +45,7 @@ elif git log -1 | grep -q "patch"; then
   BUMP_MODE="patch"
 fi
 
-if [[$PR == 'true']]
+if [[ "$PR" -eq 'true' ]]  #1 = true
 then
   echo $BUMP_MODE "version bump detected"
   bump $BUMP_MODE $OLD_VERSION
