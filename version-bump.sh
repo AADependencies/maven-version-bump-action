a1a06e569dd9404806737fc2f4bf7bacfa1410f5
ReeceRiley-aa <rhyeca.riley@aa.com>
2023-07-11 14:44:11 -0700

    Update to get main version tag

--- version-bump.sh
+++ version-bump.sh
@@ -57,14 +57,16 @@ then
   git commit -m "Bump pom.xml from $OLD_VERSION to $PR_VERSION"
   git push $REPO
 else
-  echo $BUMP_MODE "version bump detected on a push to main"
-  bump $BUMP_MODE $OLD_VERSION
+#  echo $BUMP_MODE "version bump detected on a push to main"
+#  bump $BUMP_MODE $OLD_VERSION
 #  echo "pom.xml at" $POMPATH "will be bumped from" $OLD_VERSION "to" $NEW_VERSION
-  mvn -q versions:set -DnewVersion="${NEW_VERSION}"
+#  mvn -q versions:set -DnewVersion="${NEW_VERSION}"
 #  git add $POMPATH/pom.xml
 #  REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
 #  git commit -m "Bump pom.xml from $OLD_VERSION to $NEW_VERSION"
-  git tag $NEW_VERSION
+  echo "tag version in main"
+  MAIN_VERSION=$($DIR/get-version.sh)
+  git tag $MAIN_VERSION
   git push $REPO --follow-tags
   git push $REPO --tags
 fi
