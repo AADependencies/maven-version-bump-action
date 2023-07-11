7aa398e39f69e4cc5b43b77d90dc58f4756d014a
ReeceRiley-aa <rhyeca.riley@aa.com>
2023-07-11 14:23:12 -0700

    Test only tagging main

--- version-bump.sh
+++ version-bump.sh
@@ -49,7 +49,7 @@ if [[ "${PR}" == "true" ]];
 then
   echo $BUMP_MODE "version bump detected"
   bump $BUMP_MODE $OLD_VERSION
-  PR_VERSION="${NEW_VERSION}-pr"
+  PR_VERSION="${NEW_VERSION}"
   echo "pom.xml at" $POMPATH "will be bumped from" $OLD_VERSION "to" $PR_VERSION
   mvn -q versions:set -DnewVersion=${PR_VERSION}
   git add $POMPATH/pom.xml
@@ -59,15 +59,14 @@ then
 else
   echo $BUMP_MODE "version bump detected on a push to main"
   bump $BUMP_MODE $OLD_VERSION
-  echo "pom.xml at" $POMPATH "will be bumped from" $OLD_VERSION "to" $NEW_VERSION
+#  echo "pom.xml at" $POMPATH "will be bumped from" $OLD_VERSION "to" $NEW_VERSION
   mvn -q versions:set -DnewVersion="${NEW_VERSION}"
-  git add $POMPATH/pom.xml
-  REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
-  git commit -m "Bump pom.xml from $OLD_VERSION to $NEW_VERSION"
+#  git add $POMPATH/pom.xml
+#  REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
+#  git commit -m "Bump pom.xml from $OLD_VERSION to $NEW_VERSION"
   git tag $NEW_VERSION
   git push $REPO --follow-tags
   git push $REPO --tags
-  git push origin main
 fi
 
 
